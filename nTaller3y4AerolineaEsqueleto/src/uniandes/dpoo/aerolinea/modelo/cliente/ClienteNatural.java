bb6a98d0e266a65fa9aee62303c8774e67aab490
SVargas07 <s.vargasl23@uniandes.edu.co>
2024-02-26 16:26:03 -0500

    Commit 4

---
+++ nTaller3y4AerolineaEsqueleto/src/uniandes/dpoo/aerolinea/modelo/cliente/ClienteNatural.java
@@ -0,0 +1,23 @@
+package uniandes.dpoo.aerolinea.modelo.cliente;
+
+public class ClienteNatural {
+	
+	
+	public String natutral;
+	private String nombre;
+	
+	public ClienteNatural (String nombre)
+	{
+		
+	}
+
+	public String getIdentificador()
+	{
+		return null;
+	}
+	public String getTipoCliente ()
+	{
+		return null;
+	}
+
+}
