eef61ea0e229ed70cf9eca67e3625b7b3878c26c
SVargas07 <s.vargasl23@uniandes.edu.co>
2024-02-26 15:58:30 -0500

    Commit 3

---
+++ nTaller3y4AerolineaEsqueleto/src/uniandes/dpoo/aerolinea/modelo/Vuelo.java
@@ -0,0 +1,40 @@
+package uniandes.dpoo.aerolinea.modelo;
+
+public class Vuelo {
+	
+	private String fecha;
+	
+	
+	public Vuelo (Ruta ruta, String fecha, Avion avion) 
+	{
+		
+	}
+	
+	public void getRuta()
+	{
+		
+	}
+	public void getFecha()
+	{
+		
+	}
+	public void getAvion()
+	{
+		
+	}
+	public void getTiquetes()
+	{
+		
+	}
+	
+	public int venderTiquetes(Cliente cliente, calculadoraTarifas calculadora, int cantidad)
+	{
+		return 0;
+	}
+	public boolean equals(Object obj)
+	{
+		return false;
+		
+	}
+	
+}
---
+++ nTaller3y4AerolineaEsqueleto/src/uniandes/dpoo/aerolinea/modelo/cliente/Cliente.java
@@ -0,0 +1,43 @@
+package uniandes.dpoo.aerolinea.modelo.cliente;
+
+import uniandes.dpoo.aerolinea.modelo.Vuelo;
+
+public class Cliente {
+	
+	public void cliente()
+	{
+		
+	}
+	
+	
+	public String getTipoCliente()
+	{
+		return null; 
+	}
+	
+	
+	public String getIdentificador()
+	{
+		return null;
+	}
+	
+	
+	public void agregarTiquete(Tiquete tiquete)
+	{
+		
+	}
+	
+	
+	public int calcularValorTotalTiquetes()
+	{
+		return 0;
+	}
+	
+	
+	public void usarTiquetes(Vuelo vuelo)
+	{
+		
+	}
+	
+
+}
